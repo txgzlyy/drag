6561d27459d291159122d0ff1ef84394e65d731e
txgzlyy <975663670@qq.com>
2017-05-31 10:52:24 +0800

    over

--- drag.js
+++ drag.js
@@ -4,6 +4,15 @@ function drag(id){
 	
 	obj.onmousedown = function(ev){
 		var ev = ev || event;
-		
-	}
+		var disx = ev.clientX - obj.offsetLeft;
+		var disy = ev.clientY - obj.offsetTop;
+		document.onmousemove = function(ev){
+			var ev = ev || event;
+			obj.style.left = ev.clientX - disx + 'px';
+			obj.style.top = ev.clientY - disy + 'px';
+		}
+		document.onmouseup = function(){
+			document.onmousemove = document.onmouseup = null;
+		}
+ 	}
 }
